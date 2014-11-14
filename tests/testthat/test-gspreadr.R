3e2bc226d34c0dcf134f1cbd6c0f168b00bfe4de
Joanna Zhao <joannaazhao@gmail.com>
2014-11-14 13:15:42 -0800

    Add tests and update documentation for 5d9165d

--- tests/testthat/test-gspreadr.R
+++ tests/testthat/test-gspreadr.R
@@ -1,10 +1,10 @@
 context("Sheets operations")
 
-client <- authorize()
-ss1 <- open_spreadsheet(client, "Gapminder")
+authorize()
+ss1 <- open_spreadsheet("Gapminder")
 
 test_that("List all my spreadsheets", {
-  sheets <- list_spreadsheets(client)
+  sheets <- list_spreadsheets()
   
   expect_equal(length(sheets), 2)
   expect_equal(sheets, c("Gapminder", "Gapminder by Continent"))
@@ -15,7 +15,7 @@ test_that("Open spreadsheet by title", {
   expect_equal(class(ss1), "spreadsheet")
   expect_equal(ss1$nsheets, 1)
   expect_equal(ss1$sheet_title, "Gapminder")
-  expect_error(class(open_spreadsheet(client, "Gap")), "Spreadsheet not found.")
+  expect_error(open_spreadsheet("Gap"), "Spreadsheet not found.")
 })
 
 test_that("List all my worksheets in spreadsheet", {
@@ -24,8 +24,8 @@ test_that("List all my worksheets in spreadsheet", {
 
 test_that("Get worksheet object", {
   
-  expect_equal(class(get_worksheet(client, ss1, "Sheet1")), "worksheet")
-  expect_error(get_worksheet(client, ss1, "Sheet2"), "Worksheet not found.")  
+  expect_equal(class(get_worksheet(ss1, "Sheet1")), "worksheet")
+  expect_error(get_worksheet(ss1, "Sheet2"), "Worksheet not found.")  
 })
 
 test_that("Open spreadsheet by key", {
@@ -45,36 +45,36 @@ test_that("Open spreadsheet by url", {
 })
 
 test_that("Get all values of 1 row", {
-  ws <- get_worksheet(client, ss1, "Sheet1")
+  ws <- get_worksheet(ss1, "Sheet1")
   
-  expect_equal(length(get_row(client, ws, 2)), 6)
-  expect_error(get_row(client, ws, 10000), 
+  expect_equal(length(get_row(ws, 2)), 6)
+  expect_error(get_row(ws, 10000), 
                "Specified row exceeds the number of rows contained in worksheet.")
 })
 
 test_that("Get more than 1 row", {
-  ws <- get_worksheet(client, ss1, "Sheet1")
+  ws <- get_worksheet(ss1, "Sheet1")
   
-  expect_equal(nrow(get_rows(client, ws, 2, 3)), 2)
-  expect_equal(get_rows(client, ws, 15, 12), get_rows(client, ws, 12, 15))
+  expect_equal(nrow(get_rows(ws, 2, 3)), 2)
+  expect_equal(get_rows(ws, 15, 12), get_rows(ws, 12, 15))
 })
 
 test_that("Get all values of 1 col", {
-  ws <- get_worksheet(client, ss1, "Sheet1")
+  ws <- get_worksheet(ss1, "Sheet1")
   
-  expect_equal(length(get_col(client, ws, 1)), 1705)
+  expect_equal(length(get_col(ws, 1)), 1705)
 })
 
 test_that("Get more than 1 col", {
-  ws <- get_worksheet(client, ss1, "Sheet1")
+  ws <- get_worksheet(ss1, "Sheet1")
   
-  expect_equal(ncol(get_cols(client, ws, 1, 3)), 3)
-  expect_equal(nrow(get_cols(client, ws, 1, 2)), 1705)
+  expect_equal(ncol(get_cols(ws, 1, 3)), 3)
+  expect_equal(nrow(get_cols(ws, 1, 2)), 1705)
 })
 
 test_that("Get correct dataframe", {
-  ws <- get_worksheet(client, ss1, "Sheet1")
-  my_data <- get_dataframe(client, ws)
+  ws <- get_worksheet(ss1, "Sheet1")
+  my_data <- get_dataframe(ws)
   
   expect_equal(class(my_data), "data.frame")
   expect_equal(nrow(my_data), 1705)
@@ -82,8 +82,8 @@ test_that("Get correct dataframe", {
 })
 
 test_that("Add worksheet", {
-  add_worksheet(client, ss1, "bar", 10, 10)
-  ss1 <- open_spreadsheet(client, "Gapminder")
+  add_worksheet(ss1, "bar", 10, 10)
+  ss1 <- open_spreadsheet("Gapminder")
   name_match <- "bar" %in% ss1$ws_names
   
   expect_equal(ss1$nsheets, 2)
@@ -92,10 +92,10 @@ test_that("Add worksheet", {
 })
 
 test_that("Delete worksheet", {
-  ss1 <- open_spreadsheet(client, "Gapminder")
-  ws <- get_worksheet(client, ss1, "bar")
-  del_worksheet(client, ss1, ws)
-  ss1 <- open_spreadsheet(client, "Gapminder")
+  ss1 <- open_spreadsheet("Gapminder")
+  ws <- get_worksheet(ss1, "bar")
+  del_worksheet(ss1, ws)
+  ss1 <- open_spreadsheet("Gapminder")
   name_match <- "bar" %in% ss1$ws_names
   
   expect_equal(ss1$nsheets, 1)
--- tests/testthat/test-login.R
+++ tests/testthat/test-login.R
@@ -3,20 +3,7 @@ context("login")
 test_that("Login with good/bad email and passwd", {
   email <- "gspreadr@gmail.com"
   passwd <- "gspreadrtester"
-  client <- login(email, passwd)
   
-  expect_equal(class(client), "client")
-  expect_equal(class(client$auth), "character")
   expect_error(login(email, "mypasswd"), "Incorrect username or password.")
   expect_error(login("myemail", passwd), "Incorrect username or password.")
 })
-
-
-test_that("Authorize using Oauth2.0", {
-  client <- authorize()
-  
-  expect_equal(class(client), "client")
-  expect_equal(class(client$auth)[1], "Token2.0")
-})
-
-
