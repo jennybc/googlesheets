d8854649a1d22aec24df2657343522588cf75808
Joanna Zhao <joannaazhao@gmail.com>
2015-01-22 15:10:40 -0800

    Store client id and secret in pkg options

---
+++ R/zzz.R
@@ -0,0 +1,11 @@
+.onLoad <- function(libname, pkgname) {
+  op <- options()
+  op.gspreadr <- list(
+    gspreadr.client_id = "178989665258-f4scmimctv2o96isfppehg1qesrpvjro.apps.googleusercontent.com",
+    gspreadr.client_secret = "iWPrYg0lFHNQblnRrDbypvJL"
+  )
+  toset <- !(names(op.gspreadr) %in% names(op))
+  if(any(toset)) options(op.gspreadr[toset])
+  
+  invisible()
+}
