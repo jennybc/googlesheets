cf829306284601888035b2bad5988d6387187d6e
jennybc <jenny@stat.ubc.ca>
2015-02-14 09:14:31 -0800

    make R CMD check shut up about `.` in pipelines
    this solution suggested here https://github.com/smbache/magrittr/issues/29

--- R/gspreadr.R
+++ R/gspreadr.R
@@ -8,3 +8,5 @@
 #' @name gspreadr
 #' @importFrom dplyr %>%
 NULL
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
