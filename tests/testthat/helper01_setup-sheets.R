df4ceea0e17fe7ada8ee01d7dd55eb71da21e805
jennybc <jenny@stat.ubc.ca>
2015-02-14 09:17:00 -0800

    split test helper file into two logical bits
    facilitates interactive test development, because in that context, one of them you want to source (setup-sheets) and one of them you do NOT (login)

---
+++ tests/testthat/helper01_setup-sheets.R
@@ -0,0 +1,12 @@
+## Gapminder
+gap_URL <- "https://docs.google.com/spreadsheets/d/1hS762lIJd2TRUTVOqoOP7g-h4MDQs6b2vhkTzohg8bE/"
+gap_title <- "Gapminder"
+gap_key <- "1hS762lIJd2TRUTVOqoOP7g-h4MDQs6b2vhkTzohg8bE"
+gap_ws_feed <- "https://spreadsheets.google.com/feeds/worksheets/1hS762lIJd2TRUTVOqoOP7g-h4MDQs6b2vhkTzohg8bE/private/full"
+
+
+## Public Testing Sheet (also based on Gapminder)
+pts_url <- "https://docs.google.com/spreadsheets/d/1hff6AzFAZgFdb5-onYc1FZySxTP4hlrcsPSkR0dG3qk/"
+pts_title <- "Public Testing Sheet"
+pts_key <- "1hff6AzFAZgFdb5-onYc1FZySxTP4hlrcsPSkR0dG3qk"
+pts_ws_feed <- "https://spreadsheets.google.com/feeds/worksheets/1hff6AzFAZgFdb5-onYc1FZySxTP4hlrcsPSkR0dG3qk/public/full"
--- tests/testthat/helper02_login.R
+++ tests/testthat/helper02_login.R
@@ -1,16 +1,3 @@
-## Gapminder
-gap_URL <- "https://docs.google.com/spreadsheets/d/1hS762lIJd2TRUTVOqoOP7g-h4MDQs6b2vhkTzohg8bE/"
-gap_title <- "Gapminder"
-gap_key <- "1hS762lIJd2TRUTVOqoOP7g-h4MDQs6b2vhkTzohg8bE"
-gap_ws_feed <- "https://spreadsheets.google.com/feeds/worksheets/1hS762lIJd2TRUTVOqoOP7g-h4MDQs6b2vhkTzohg8bE/private/full"
-
-
-## Public Testing Sheet (also based on Gapminder)
-pts_url <- "https://docs.google.com/spreadsheets/d/1hff6AzFAZgFdb5-onYc1FZySxTP4hlrcsPSkR0dG3qk/"
-pts_title <- "Public Testing Sheet"
-pts_key <- "1hff6AzFAZgFdb5-onYc1FZySxTP4hlrcsPSkR0dG3qk"
-pts_ws_feed <- "https://spreadsheets.google.com/feeds/worksheets/1hff6AzFAZgFdb5-onYc1FZySxTP4hlrcsPSkR0dG3qk/public/full"
-
 ## current hack to test as authenticted user
 ## requires GSPREADR_PASSWORD environment variable
 
