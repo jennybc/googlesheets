c3a3bdeaa6e3991bfaca41f7b6671c540752f25d
jennybc <jenny@stat.ubc.ca>
2015-05-31 17:03:34 -0700

    update tests of registration functions

--- tests/testthat/test-gs-register.R
+++ tests/testthat/test-gs-register.R
@@ -1,86 +1,110 @@
 context("register sheets")
 
-test_that("Spreadsheet can be ID'd via URL, key, title, ws_feed or ss", {
+iris_pvt_url %>%
+  gs_url(verbose = FALSE) %>%
+  saveRDS("for_reference/iris_pvt_googlesheet.rds")
 
-  ## NOTE: we've got to look for stuff we (gspreadr) own here, because this is
-  ## all about the spreadsheets feed
+GAP_KEY %>%
+  gs_key(verbose = FALSE) %>%
+  saveRDS("for_reference/gap_googlesheet.rds")
 
-  expect_equal_to_iris_identify <- function(x, method = NULL) {
-    expect_equal_to_reference(identify_ss(x), "iris_identify_ss.rds")
-  }
+pseudo_expect_equal_to_reference <- function(x, ref) {
+  ref_rds <- file.path("for_reference", paste0(ref, "_googlesheet.rds"))
+  ref <- readRDS(ref_rds)
+  stable_bits <- c("sheet_key", "sheet_title", "n_ws",
+                   "author", "email", "version")
+  inherits(x, "googlesheet") &&
+    identical(x[stable_bits], ref[stable_bits])
+}
 
-  ## let identify_ss() determine the method
-  #expect_equal_to_iris_identify(iris_pvt_url)
-  #expect_equal_to_iris_identify(iris_pvt_key)
-  #expect_equal_to_iris_identify(iris_pvt_title)
-  #expect_equal_to_iris_identify(iris_pvt_ws_feed)
-  #iris_gs <- identify_ss(iris_pvt_key)
-  #expect_equal_to_iris_identify(iris_gs)
+test_that("Spreadsheet can be registered via title", {
 
-  ## explicitly provide the correct method
-  #expect_equal_to_iris_identify(iris_pvt_url, method = "url")
-
-  #expect_equal_to_iris_identify(iris_pvt_key, method = "key")
-  #expect_equal_to_iris_identify(iris_pvt_title, method = "title")
-  #expect_equal_to_iris_identify(iris_pvt_ws_feed, method = "ws_feed")
-  #expect_equal_to_iris_identify(iris_gs, method = "ss")
-
-  ## request NO verification
-  expect_iris_key <- function(x) {
-    expect_equal(identify_ss(x, verify = FALSE)$sheet_key, iris_pvt_key)
-  }
-
-  #expect_iris_key(iris_pvt_url)
-  #expect_iris_key(iris_pvt_ws_feed)
-  #expect_iris_key(iris_pvt_key)
-  #expect_iris_key(iris_gs)
-  ## note this "works" but proclaims the title as the key
-  #expect_equal(identify_ss(
-    #iris_pvt_title, verify = FALSE)$sheet_key, iris_pvt_title)
+  pseudo_expect_equal_to_reference(iris_pvt_title %>% gs_title(), "iris_pvt")
 
 })
 
-test_that("Bad spreadsheet ID throws informative error", {
+test_that("Spreadsheet can be registered via key", {
 
-  ## errors that prevent attempt to identify spreadsheet
-  #expect_error(identify_ss(4L), "must be character")
-  #expect_error(identify_ss(c("Gapminder", "Gapminder")), "must be of length 1")
+  ## sheet owned by gspreadr = authenticated user but not "published to web"
+  pseudo_expect_equal_to_reference(iris_pvt_key %>% gs_key(), "iris_pvt")
+  pseudo_expect_equal_to_reference(
+    iris_pvt_key %>% gs_key(lookup = FALSE, visibility = "private"), "iris_pvt")
 
-  ## explicit declaration of an invalid method
-  #expect_error(identify_ss(pts_key, method = "eggplant"), "Error in match.arg")
+  ## sheet owned by rpackagetest and "published to web"
+  pseudo_expect_equal_to_reference(GAP_KEY %>% gs_key(), "gap")
+  pseudo_expect_equal_to_reference(GAP_KEY %>% gs_key(lookup = FALSE), "gap")
+  pseudo_expect_equal_to_reference(GAP_KEY %>% gs_key(visibility = "public"),
+                                   "gap")
+  pseudo_expect_equal_to_reference(GAP_KEY %>% gs_key(visibility = "private"),
+                                   "gap")
+  pseudo_expect_equal_to_reference(
+    GAP_KEY %>% gs_key(lookup = FALSE, visibility = "private"), "gap")
+  pseudo_expect_equal_to_reference(
+    GAP_KEY %>% gs_key(lookup = FALSE, visibility = "public"), "gap")
 
-  ## incompatible choices for method and verify
-  #expect_error(identify_ss("eggplant", method = "title", verify = FALSE),
-               #"must look up the title")
+})
+
+test_that("Spreadsheet can be registered via URL", {
+
+  ## sheet owned by gspreadr = authenticated user but not "published to web"
+  pseudo_expect_equal_to_reference(iris_pvt_url %>% gs_url(), "iris_pvt")
+  pseudo_expect_equal_to_reference(iris_pvt_url %>% gs_url(lookup = TRUE),
+                                   "iris_pvt")
+  pseudo_expect_equal_to_reference(
+    iris_pvt_url %>% gs_url(lookup = FALSE, visibility = "private"), "iris_pvt")
+
+  ## sheet owned by rpackagetest and "published to web"
+  pseudo_expect_equal_to_reference(GAP_URL %>% gs_url(), "gap")
+  pseudo_expect_equal_to_reference(GAP_URL %>% gs_url(lookup = FALSE), "gap")
+  pseudo_expect_equal_to_reference(
+    GAP_URL %>% gs_url(lookup = FALSE, visibility = "private"), "gap")
+
+})
+
+test_that("Spreadsheet can be registered via ws_feed", {
+
+  ## sheet owned by gspreadr = authenticated user but not "published to web"
+  pseudo_expect_equal_to_reference(iris_pvt_ws_feed %>% gs_ws_feed(),
+                                   "iris_pvt")
+  pseudo_expect_equal_to_reference(
+    iris_pvt_ws_feed %>% gs_ws_feed(lookup = FALSE), "iris_pvt")
+
+  ## sheet owned by rpackagetest and "published to web"
+  pseudo_expect_equal_to_reference(GAP_WS_FEED %>% gs_ws_feed(), "gap")
+  pseudo_expect_equal_to_reference(
+    GAP_WS_FEED %>% gs_ws_feed(lookup = FALSE), "gap")
+
+})
+
+test_that("Spreadsheet can be registered via googlesheet", {
+
+  iris_ss <- iris_pvt_key %>% gs_key()
+
+  ## sheet owned by gspreadr = authenticated user but not "published to web"
+  pseudo_expect_equal_to_reference(iris_ss %>% gs_gs(), "iris_pvt")
+
+  ## sheet owned by rpackagetest and "published to web"
+  pseudo_expect_equal_to_reference(gs_gap() %>% gs_gs(), "gap")
+  pseudo_expect_equal_to_reference(gs_gap() %>% gs_gs(visibility = "private"),
+                                   "gap")
+
+})
+
+test_that("Bad spreadsheet ID throws error", {
+
+  expect_error(gs_key(4L), "is.character")
+  expect_error(gs_title(rep("Gapminder", 2)), "length")
+  expect_error(gs_gs("Gapminder"), "googlesheet")
 
   ## errors caused by well-formed input that refers to a nonexistent spreadsheet
-  #expect_error(identify_ss("spatula"), "doesn't match")
-
-  nonexistent_ws_feed <- sub(iris_pvt_key, "flyingpig", iris_pvt_ws_feed)
-  expect_error(gs_ws_feed(nonexistent_ws_feed), "doesn't match")
-  expect_error(gs_ws_feed(nonexistent_ws_feed), "doesn't match")
-
+  expect_error(gs_title("spatuala"), "doesn't match")
+  expect_error(gs_key("flyingpig"), "doesn't match")
   nonexistent_url <- sub(iris_pvt_key, "flyingpig", iris_pvt_url)
   expect_error(gs_url(nonexistent_url), "doesn't match")
+  nonexistent_ws_feed <- sub(iris_pvt_key, "flyingpig", iris_pvt_ws_feed)
+  expect_error(gs_ws_feed(nonexistent_ws_feed), "doesn't match")
 
-  nonexistent_key <- "flyingpig"
-  expect_error(gs_key(nonexistent_key), "doesn't match")
-
-})
-
-test_that("Spreadsheet can be registered via URL, key, title, ws_feed or ss", {
-
-  expect_googlesheet <- function(x) expect_is(x, "googlesheet")
-
-  expect_googlesheet(gs_ws_feed(iris_pvt_ws_feed))
-  expect_googlesheet(gs_title(iris_pvt_title))
-  expect_googlesheet(gs_key(iris_pvt_key))
-  expect_googlesheet(gs_url(iris_pvt_url))
-  # am I going to create gs_gs()?
-  #iris_gs <- identify_ss(iris_pvt_key)
-  #expect_googlesheet(register_ss(iris_gs))
-
-})
+  })
 
 test_that("We get correct number and titles of worksheets", {
 
